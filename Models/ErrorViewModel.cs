d0921b1acb5f5bf59ce15584a15e4ec78d5184a6
Selva <selva.syes@gmail.com>
2022-11-11 13:35:22 +0530

    All Comitted

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PipelineDeploy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
